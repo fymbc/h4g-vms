d7dc780dda6e3222a5819eaa109ce9784467c63b
fymbc <weixuan2606@gmail.com>
2024-02-07 20:11:26 +0800

    Add dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Login from './login'
+import Dashboard from './dashboard'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
       <Routes>
           <Route path="/" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path="/dashboard" element={<Dashboard setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
       </Routes>
       </BrowserRouter>
     </div>
